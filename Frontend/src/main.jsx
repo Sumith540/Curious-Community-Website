b02e74b527adf8541f063e906fab878a6b3a8e5c
Sumith Das <129795841+Sumith540@users.noreply.github.com>
2024-10-15 19:06:50 +0530

    Update main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import "./App.css";
+import Navbar from './Navbar';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+function App() {
+  return (
+    <>
+      <Navbar />
+      <div className="font-mono text-pretty text-[20px] font-bold">
+        <h1>Curious Developers Community</h1>
+      </div>
+      <section id="section1" className="h-screen bg-blue-100">
+        <h2>Section 1</h2>
+      </section>
+      <section id="section2" className="h-screen bg-green-100">
+        <h2>Section 2</h2>
+      </section>
+      <section id="section3" className="h-screen bg-yellow-100">
+        <h2>Section 3</h2>
+      </section>
+    </>
+  );
+}
+
+export default App;
